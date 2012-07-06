99d8b5e9aff7f79c326b6efe269be7d455fc03bd
huangyingw@linux_programming <huangyingw@gmail.com>
2012-07-06 12:21:04 -0500

    n

--- src/Test.java
+++ src/Test.java
@@ -4,18 +4,6 @@ public class Test {
 		str = "welcome";
 	}
 
-	public void changeStr1(String str) {
-		str = "welcome";
-	}
-
-	public void changeStr2(int str) {
-		str = 34;
-	}
-
-	public void changeStr3(Integer str) {
-		str = 34;
-	}
-
 	public static void main(String[] args) {
 		String str = "1234";
 		changeStr(str);
@@ -26,8 +14,33 @@ public class Test {
 		int temp = 12;
 		test.changeStr2(temp);
 		System.out.println(temp);
-		Integer temp1 = 12;
+		Integer temp1 = new Integer(12);
 		test.changeStr3(temp1);
 		System.out.println(temp1);
+		myClass temp2 = new myClass(12);
+		test.changeStr4(temp2);
+		System.out.println(temp2);
+		test.changeStr5(temp2);
+		System.out.println(temp2);
+	}
+
+	public void changeStr1(String str) {
+		str = "welcome";
+	}
+
+	public void changeStr2(int str) {
+		str = 34;
+	}
+
+	public void changeStr3(Integer str) {
+		str = new Integer(34);
+	}
+
+	public void changeStr4(myClass str) {
+		str = new myClass(34);
+	}
+
+	public void changeStr5(myClass str) {
+		str.num = 34;
 	}
 }
---
+++ src/myClass.java
@@ -0,0 +1,12 @@
+public class myClass {
+	int num;
+
+	public myClass(int num) {
+		super();
+		this.num = num;
+	}
+
+	public String toString() {
+		return Integer.toString(num);
+	}
+}
